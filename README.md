555af54648becd7f2044d3a396b114b41f9b78b3
TARUNCHAUHAN3010 <tarunchauhan3010@gmail.com>
2024-06-03 12:21:09 +0530

    new changes

--- README.md
+++
@@ -1,3 +0,0 @@
-# QR-Code-generetor
-this project is genetating QR Codes
-Author - Tarun singh Chauhan
---
+++ qr-code-generator/src/App.jsx
@@ -0,0 +1,14 @@
+import QrCode from './componants/Qrcode'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='bg'>
+        <h1 className='title'>QR CODE GENERATOR</h1>
+     <QrCode/> 
+    </div>
+  )
+}
+
+export default App
